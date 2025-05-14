648c9e614dcba413ac6ed25af7d660ed726838f0
Fanny <fannypeterssons@icloud.com>
2025-05-14 21:08:32 +0200

    Update index.js
    Add main and remove div outside

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Lånekalkylator</title>
       </Head>
-      <h1>Välkommen till lånekalkylator</h1>
-      <LoanCalculator />
-    </div>
+      <main>
+        <h1>Välkommen till lånekalkylator</h1>
+        <LoanCalculator />
+      </main>
+    </>
   );
 }
